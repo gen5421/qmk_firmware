67ab0ebc396b2efc5f93ac5584f87c82e70f2dfc
gen5421 <34391322+gen5421@users.noreply.github.com>
2017-12-12 17:18:43 +0900

    Update keymap.c

--- keyboards/lets_split/keymaps/gen5421/keymap.c
+++ keyboards/lets_split/keymaps/gen5421/keymap.c
@@ -25,8 +25,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = KEYMAP( \
   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_F14,  KC_F13,  KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    \
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ESC,  _______, KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, \
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_TAB,  KC_ENT,  KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, \
+  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ESC,  KC_ENT,  KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, \
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_TAB,  _______, KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, \
   KC_LCTL, KC_LALT, KC_LGUI, RAISE,   KC_LSFT, KC_BSPC, KC_SPC,  KC_RSFT, LOWER,    KC_RGUI, KC_RALT, KC_RCTL  \
 ),
 
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = KEYMAP( \
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, \
+  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_1,    KC_2,    KC_3,    KC_RGHT, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
 
